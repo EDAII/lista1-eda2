f9e0b7885b1b31cea2c9d3588a66720e2b6fb4ad
Anderson Silva <andersoonn.ams@gmail.com>
2018-08-23 22:40:19 -0300

    binary search

--- busca.py
+++ busca.py
@@ -5,33 +5,52 @@ from random import shuffle
 def create_archive(size):
     
     file_asc = open('file_asc.txt','w') 
-    file_dsc = open('file_dsc.txt','w') 
-    file_ran = open('file_ran.txt','w')
-    
-    aux_desc = size -1
+   
 
     for aux_asc in range(size):
 
         file_asc.write(str(aux_asc)+'\n')
         aux_asc+=1
 
-        file_dsc.write(str(aux_desc)+'\n')
-        aux_desc-= 1
+    file_asc.close()
+
+def sequencial_search(key):
+
+    file = open('file_asc.txt','r') 
+
+    for line in file.readlines():
+        if (line == key+'\n'): 
+            print('key found')
+            break
+
+# Python 2.7
+# 1	def binarySearch(alist, item):
+# 2	    first = 0
+# 3	    last = len(alist)-1
+# 4	    found = False
+# 5	
+# 6	    while first<=last and not found:
+# 7	        midpoint = (first + last)//2
+# 8	        if alist[midpoint] == item:
+# 9	            found = True
+# 10	        else:
+# 11	            if item < alist[midpoint]:
+# 12	                last = midpoint-1
+# 13	            else:
+# 14	                first = midpoint+1
+# 15	
+# 16	    return found
+
+
+
+# def binary_search(key):
+
+#     file = open('file_asc.txt','r') 
+#     first = 0
+#     last = len
+
 
     
-    x = [[i] for i in range(size)]
-    shuffle(x)
-
-    for aux_ran in x:
-        # file_ran.write(str(aux_ran)+'\n')
-        int(aux_ran)
-        print(str(aux_ran))
-        # file_ran.write(str(aux_ran)+'\n')
-
-    file_asc.close()
-    file_dsc.close()
-    file_ran.close()
-
    
 
 def main():
@@ -42,7 +61,8 @@ def main():
 
     # if len(sys.argv) == 1:
 
-    create_archive(5000)
+    create_archive(100)
+    sequencial_search('10')
         
 
 
